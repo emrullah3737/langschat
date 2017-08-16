4fcc65a02f6236058b8b78a1541678e297b5240d
Emrullah Karatas <emrullah@efabrika.com>
2017-08-16 13:51:17 +0300

    log

--- config/upload.js
+++ config/upload.js
@@ -7,6 +7,7 @@ const storage = multer.diskStorage({
     cb(null, 'public/uploads/');
   },
   filename: (req, file, cb) => {
+    console.log(file);
     const extend = file.originalname.split('.')[1];
     cb(null, `${file.fieldname}-${Date.now()}.${extend}`);
   },
